b878317408f0080d6ef8664ad5b15e499bca9c39
Mohamadreza <mohamadrezafathibesharat@gmail.com>
2020-05-17 23:36:20 +0430

    Configure startupfile.cs

--- TakkalaDigitalShopping/Startup.cs
+++ TakkalaDigitalShopping/Startup.cs
@@ -16,6 +16,8 @@ namespace TakkalaDigitalShopping
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,14 +29,15 @@ namespace TakkalaDigitalShopping
             }
 
             app.UseRouting();
-
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello Big Kitty!");
-                });
+
+                endpoints.MapDefaultControllerRoute();
+
             });
+
+            
         }
     }
 }
