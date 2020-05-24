1179f087e8836e2c4471ecaae01ecde94b189a3f
Mohamadreza <mohamadrezafathibesharat@gmail.com>
2020-05-24 19:06:45 +0430

    Add DbContext file

---
+++ TakkalaDigitalShopping/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TakkalaDigitalShopping.Data.Models;
+
+namespace TakkalaDigitalShopping.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<SubGroup> subGroups { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
